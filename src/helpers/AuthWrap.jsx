25bd99c78ddcb1312425c0bec6e92687681b3aa2
Yevhen Shmatko <yevhen.shmatko@gmail.com>
2021-01-08 21:23:43 +0200

    reload

--- src/helpers/AuthWrap.jsx
+++ src/helpers/AuthWrap.jsx
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
-import routes from '../routes';
 
-function AuthWrap({ auth = false, path, component, goTo }) {
+const AuthWrap = ({ auth, path, component, goTo }) => {
   return auth ? (
     <Route exact path={path} component={component} />
   ) : (
     <Redirect to={goTo} />
   );
-}
+};
 
 export default AuthWrap;
